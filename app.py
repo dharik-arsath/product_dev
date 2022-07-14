56dc80dd2e7903539e1169b83d3748ad63d1c164
Dharik Arsath <dharikarsath12@gmail.com>
2022-07-15 02:13:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from libpy import DataProvider, PreProcessor, Modeller
+import pandas as pd
+
+data = pd.read_csv(
+    "amazon.csv",
+    usecols=['product_id', 'product_title', 'star_rating',
+                'review_body', 'review_date'])
+
+
+def convert_dtype_data(data: 'pd.DataFrame', cols_dict: dict):
+    for each in cols_dict.keys():
+        data.loc[:, each] = data.loc[:, each].astype(cols_dict[each])
+
+    data.loc[:, 'review_date'] = pd.to_datetime(data.review_date, errors='coerce')
+
+
+# cols_dict = {
+#     #     'marketplace' : 'category',
+#     #     'product_category' : 'category',
+#     'vine': 'category',
+#     'verified_purchase': 'category',
+# }
+
+# convert_dtype_data(data, cols_dict)
+data['star_rating'] = pd.to_numeric(data.star_rating, downcast='integer')
+
+
+new_data = data.copy()
+new_data.dropna(inplace=True)
+
+
+st.title("Product Review Analyzer")
+prod_id = st.selectbox("Product_id", data.product_id)
+
+
+new_data = data
+provider = DataProvider(new_data)
+
+revs = provider.get_data(prod_id)
+
+preprocess = PreProcessor()
+revs = preprocess.lower_text(revs, column="review_body")
+
+model = Modeller()
+product_title = new_data.loc[new_data.product_id == prod_id].product_title.iloc[0]
+print(f"Reviews For {product_title}")
+
+
+tops = model.build_model(revs.review_body, n_components=5)
+for each in tops:
+    st.text(each)
+    
---
+++ libpy.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import spacy
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+
+class DataProvider:
+    def __init__(self, data, positive_threshold=3):
+        self._data = data
+        self._threshold = positive_threshold
+
+    def get_positive_review(self, product_id='all'):
+        if product_id == 'all':
+            data = self._data.loc[self._data.loc[:, 'star_rating'] >= self._threshold]
+        else:
+            data = self._data.loc[self._data.loc[:, 'product_id'] == product_id]
+            data = data.loc[data.loc[:, 'star_rating'] >= self._threshold]
+
+        return data
+
+    def get_negative_review(self, product_id='all'):
+        if product_id == 'all':
+            data = self._data.loc[self._data.loc[:, 'star_rating'] <= self._threshold]
+        else:
+            data = self._data.loc[self._data.loc[:, 'product_id'] == product_id]
+            data = data.loc[data.loc[:, 'star_rating'] < self._threshold]
+
+        return data
+
+    def get_data(self, product_id):
+        return self._data.loc[self._data.loc[:, 'product_id'] == product_id]
+
+class PreProcessor:
+    def __init__(self):
+        pass
+
+    def lower_text(self, data, column):
+        data.loc[:, column] = data.loc[:, column].str.lower()
+        return data
+
+
+class Processor:
+    def __init__(self):
+        pass
+
+    def extract_tokens_plus_meta(self, doc: spacy.tokens.doc.Doc):
+        """Extract tokens and metadata from individual spaCy doc."""
+
+        text = [
+            i.lemma_ for i in doc if not any([i.is_punct, i.is_bracket, i.is_stop])
+        ]
+
+        return " ".join(text)
+
+    # add custom stop words
+
+
+class Modeller:
+    def __init__(self, load_small=True):
+
+        if load_small:
+            self.nlp = spacy.load("en_core_web_sm")
+        else:
+            self.nlp = spacy.load("en_core_web_lg")
+
+        self.processor = Processor()
+
+    def _get_top_words(self, model, feature_names, n_top_words):
+        topics = list()
+        for topic_idx, topic in enumerate(model.components_):
+            message = "Topic #%d: " % topic_idx
+            message += ", ".join([feature_names[i]
+                                  for i in topic.argsort()[:-n_top_words - 1:-1]])
+            topics.append(message)
+
+        return topics
+
+    def _extract_token(self, text):
+        docs = list(self.nlp.pipe(text))
+        for index, doc in enumerate(docs):
+            docs[index] = self.processor.extract_tokens_plus_meta(doc)
+
+        return docs
+
+    def apply_transformer(self, docs):
+        cv = CountVectorizer(lowercase=False, ngram_range=(3, 4), analyzer="word", decode_error="ignore")
+        cv.fit(docs)
+        return cv, cv.get_feature_names_out()
+
+    def _train_topic_model(self, X, n_components=5):
+        nmf = NMF(n_components=n_components, max_iter=500)
+        nmf.fit(X)
+        return nmf
+
+    def build_model(self, text, n_components=3, top_words=8):
+        docs = self._extract_token(text)
+        cvt, feature_names = self.apply_transformer(docs)
+        X = cvt.transform(docs)
+        nmf = self._train_topic_model(X, n_components)
+        topics = self._get_top_words(nmf, feature_names, top_words)
+        return topics
+
+
+
+class ProductAnalyzer:
+    def __init__(self):
+        self.model = Modeller(load_small=True)
+
+    def get_topics(self, data):
+        return self.model.build_model(data)
+
